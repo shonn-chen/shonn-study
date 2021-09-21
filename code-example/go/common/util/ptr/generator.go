3860d5dd28a459c3db334e3a3e89232b913fac1a
xiaohongchen <xiaohong.chen@shopee.com>
2021-09-21 11:51:39 +0800

    Add util/ptr/generator tool

---
+++ code-example/go/common/util/ptr/generator.go
@@ -0,0 +1,45 @@
+package ptr
+
+func StringPtr(val string) *string {
+	return &val
+}
+
+func IntPtr(val int) *int {
+	return &val
+}
+
+func Int8Ptr(val int8) *int8 {
+	return &val
+}
+
+func Int16Ptr(val int16) *int16 {
+	return &val
+}
+
+func Int32Ptr(val int32) *int32 {
+	return &val
+}
+
+func Int64Ptr(val int64) *int64 {
+	return &val
+}
+
+func UIntPtr(val uint) *uint {
+	return &val
+}
+
+func UInt8Ptr(val uint8) *uint8 {
+	return &val
+}
+
+func UInt16Ptr(val uint16) *uint16 {
+	return &val
+}
+
+func UInt32Ptr(val uint32) *uint32 {
+	return &val
+}
+
+func UInt64Ptr(val uint64) *uint64 {
+	return &val
+}
